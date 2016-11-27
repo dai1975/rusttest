2cda4b9ff678362f7486fd020fbdeafc7c6823e0
KANDA Daisuke <dai1975@gmail.com>
2016-11-27 10:24:43 +0900

    impl tokio

--- nettest/src/main.rs
+++ nettest/src/main.rs
@@ -8,17 +8,22 @@ mod mio_test;
 extern crate mioco;
 mod mioco_test;
 
+extern crate futures;
+extern crate tokio_core;
+mod tokio_test;
+
 fn main() {
    let args: Vec<String> = env::args().collect();
    if args.len() < 2 {
       println!("{} <commands>", args[0]);
-      println!("commands are: net, mio, mioco");
+      println!("commands are: net, mio, mioco, tokio");
    }
 
    let r = match args[1].as_ref() {
       "net"   => net_test::main(),
       "mio"   => mio_test::main(),
       "mioco" => mioco_test::main(),
+      "tokio" => tokio_test::main(),
       _ => Err(format!("unknown test: {}", args[1]))
    };
    let _ = r.map_err(|s|println!("{}",s));
